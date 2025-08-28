0dac93f6baa36000f3f85e79ba3f4fec7f1acf83
moohilmy <hilmy7132@gmail.com>
2025-08-28 12:16:57 +0300

    fix: Toggle comments for ApproveBtn and EditBtn components to adjust rendering

--- app/(admin-layout)/app-control/[randomString]/admin/[adminID]/approve-products/page.tsx
+++ app/(admin-layout)/app-control/[randomString]/admin/[adminID]/approve-products/page.tsx
@@ -3,8 +3,8 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { getAllNeedApproveProducts } from "@/utils/getAllNeedApproveProducts";
 import Link from "next/link";
-// import ApproveBtn from "@/app/(admin-layout)/components/Btns/ApproveBtn"
-import EditBtn from "@/app/(admin-layout)/components/Btns/EditBtn";
+import ApproveBtn from "@/app/(admin-layout)/components/Btns/ApproveBtn"
+// import EditBtn from "@/app/(admin-layout)/components/Btns/EditBtn";
 
 export default async function Page() {
   const cookiesStore = await cookies();
@@ -62,12 +62,12 @@ export default async function Page() {
               </td>
               <td className="p-3 border-b">
                 <div className="flex gap-2 flex-row justify-center">
-                  {/* <ApproveBtn
+                  <ApproveBtn
                     productID={p._id}
                     adminID={adminID.value}
                     token={token.value}
-                  /> */}
-                  <EditBtn productID={p._id} adminID={adminID.value} />
+                  />
+                  {/* <EditBtn productID={p._id} adminID={adminID.value} /> */}
                 </div>
               </td>
             </tr>
